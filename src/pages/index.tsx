255975910f2fc0e57d345b098d8878ca29f13194
sinuheshinbr <sinuheshin@gmail.com>
2023-02-07 15:48:00 -0300

    testing with rd3t

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,132 @@
+import Tree from "react-d3-tree";
 import { type NextPage } from "next";
 import { jsPDF } from "jspdf";
 import html2canvas from "html2canvas";
 import { useRef } from "react";
+
+const orgChart = {
+  name: "CEO",
+  children: [
+    {
+      name: "Manager",
+      attributes: {
+        department: "Production",
+      },
+      children: [
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Assembly",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 const Home: NextPage = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -15,9 +140,8 @@ const Home: NextPage = () => {
     pdf.addImage(canvas, "png", 0, 0, width, height);
     pdf.save();
   };
-
   return (
-    <div className="overflow-scroll">
+    <>
       <button
         onClick={() => {
           void generatePdf();
@@ -26,18 +150,15 @@ const Home: NextPage = () => {
       >
         gerar pdf
       </button>
-
-      <div ref={ref} className="flex min-w-fit">
-        {Array.from(Array(100).keys()).map((k) => (
-          <div
-            key={k}
-            className="flex h-32 w-32 shrink-0 items-center justify-center border"
-          >
-            Texto
-          </div>
-        ))}
+      <div ref={ref} id="treeWrapper" className="h-[600px] w-[3000px]">
+        <Tree
+          zoomable={false}
+          orientation="vertical"
+          translate={{ x: 1400, y: 100 }}
+          data={orgChart}
+        />
       </div>
-    </div>
+    </>
   );
 };
 
